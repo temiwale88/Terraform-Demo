f3d638b32ae529cdd05049c63f2adbf61285197d
Elijah Adeoye <temiwale88@gmail.com>
2021-07-05 23:15:24 -0700

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "ASP-TerraForm" {
 }
 
 resource "azurerm_app_service" "AS-Terraform" {
-  name                = "app-service-terraform"
+  name                = "elijah-app-service-terraform"
   location            = azurerm_resource_group.RG-Terraform.location
   resource_group_name = azurerm_resource_group.RG-Terraform.name
   app_service_plan_id = azurerm_app_service_plan.ASP-TerraForm.id
@@ -37,16 +37,16 @@ resource "azurerm_app_service" "AS-Terraform" {
 }
 
 resource "azurerm_sql_server" "test" {
-  name                         = "terraform-sqlserver"
+  name                         = "elijah-terraform-sqlserver"
   resource_group_name          = azurerm_resource_group.RG-Terraform.name
   location                     = azurerm_resource_group.RG-Terraform.location
   version                      = "12.0"
-  administrator_login          = "houssem"
+  administrator_login          = "elijah"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
 resource "azurerm_sql_database" "test" {
-  name                = "terraform-sqldatabase"
+  name                = "elijah-terraform-sqldatabase"
   resource_group_name = azurerm_resource_group.RG-Terraform.name
   location            = azurerm_resource_group.RG-Terraform.location
   server_name         = azurerm_sql_server.test.name
